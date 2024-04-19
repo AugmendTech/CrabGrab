ff5a448dd3b2d6830b5e1924e50cde157e744ce3
liam <liam@augmend.com>
2024-04-19 13:03:30 -0700

    update windows docs

--- docs/windows_docs/trait.impl/core/ops/drop/trait.Drop.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"crabgrab":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"crabgrab/platform/windows/struct.ImplCaptureStream.html\" title=\"struct crabgrab::platform::windows::ImplCaptureStream\">WindowsCaptureStream</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
