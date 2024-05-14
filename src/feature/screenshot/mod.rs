a98ac38b56d07ba90c6f82cbf757a3d7f66fc750
liam <liam@augmend.com>
2024-05-14 14:33:41 -0700

    bump version and impl Display/Error for ScreenshotError

--- src/feature/screenshot/mod.rs
+++ src/feature/screenshot/mod.rs
@@ -1,4 +1,6 @@
 mod platform;
+use std::{error::Error, fmt::Display};
+
 pub use platform::take_screenshot;
 
 #[derive(Debug)]
@@ -9,3 +11,25 @@ pub enum ScreenshotError {
 
 unsafe impl Send for ScreenshotError {}
 unsafe impl Sync for ScreenshotError {}
+
+impl Display for ScreenshotError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Other(error) => f.write_fmt(format_args!("ScreenshotError::Other({})", error)),
+        }
+    }
+}
+
+impl Error for ScreenshotError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+
+    fn description(&self) -> &str {
+        "description() is deprecated; use Display"
+    }
+
+    fn cause(&self) -> Option<&dyn Error> {
+        self.source()
+    }
+}
