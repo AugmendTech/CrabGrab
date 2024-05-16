527428f800568ed139effe2fb09d28b0412aed19
liam <liam@augmend.com>
2024-05-16 12:52:25 -0700

    fix CapturableWindow::from_native_window_handle() to not require &self

--- src/platform/windows/capturable_content.rs
+++ src/platform/windows/capturable_content.rs
@@ -236,7 +236,7 @@ impl WindowsCapturableWindowNativeWindowHandle for CapturableWindow {
         self.impl_capturable_window.0
     }
 
-    fn from_native_window_handle(&self, window_handle: HWND) -> Result<Self, CapturableContentError> {
+    fn from_native_window_handle(window_handle: HWND) -> Result<Self, CapturableContentError> {
         if !unsafe { IsWindow(window_handle).as_bool() } {
             return Err(CapturableContentError::Other(format!("HWND {:016X} is not a window", window_handle.0)));
         }
