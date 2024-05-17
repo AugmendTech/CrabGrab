4661a9deec0b247a41bf520f2737a03e396d7cde
liam <liam@augmend.com>
2024-05-17 15:48:50 -0700

    revert example to finder

--- examples/capture_window.rs
+++ examples/capture_window.rs
@@ -17,7 +17,7 @@ fn main() {
         }
         let window = content.windows().filter(|window| {
             let app_identifier = window.application().identifier();
-            window.title().len() != 0 && (app_identifier.to_lowercase().contains("terminal") || app_identifier.to_lowercase().contains("explorer"))
+            window.title().len() != 0 && (app_identifier.to_lowercase().contains("finder") || app_identifier.to_lowercase().contains("explorer"))
         }).next();
         match window {
             Some(window) => {
